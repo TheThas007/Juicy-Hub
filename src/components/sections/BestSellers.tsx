91c24d8e8ce6021d75011292367839aef46c320b
Selvanantham Thasmilan <thethas007@gmail.com>
2026-06-05 11:07:02 +0530

    Reduce mobile image height for Best Sellers

--- src/components/sections/BestSellers.tsx
+++ src/components/sections/BestSellers.tsx
@@ -7,7 +7,7 @@ export function BestSellers() {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 grid-rows-auto md:grid-rows-2 gap-6 md:h-[600px]">
           {/* Falooda - tall card */}
-          <div className="md:col-span-1 md:row-span-2 relative group rounded-3xl overflow-hidden bg-black text-white p-8 flex flex-col justify-end min-h-[400px]">
+          <div className="md:col-span-1 md:row-span-2 relative group rounded-3xl overflow-hidden bg-black text-white p-8 flex flex-col justify-end min-h-[250px] md:min-h-[400px]">
             <img
               className="absolute inset-0 w-full h-full object-cover opacity-50 group-hover:scale-110 transition-transform duration-1000"
               src="https://lh3.googleusercontent.com/aida-public/AB6AXuCNc7mjysQw-pBtuH4QTjNSChAPO9xmD_tC5UdqufnP4dVydgZvjyGratI2XKd4teXdXlmJQ2nohtsAwzIrCXjHIUdDp_OhoMz171nMtmCibCXxSDCSCbKdsUHEJJE9OjeapeEAMLoAzANAUzmI1ud1X_80ZzTwSl5vQuT9B8hm6lDi436eNoWQf4jDj4KQEyHFc9uVOyvetXolYaF1o9HywAmvOstM6E4OAQmphRxzF7qgtdqeL8d7kw_HilyUIdgJ-W6h8ukhvVk"
@@ -35,7 +35,7 @@ export function BestSellers() {
           </div>
 
           {/* Masala Tea */}
-          <div className="relative group rounded-3xl overflow-hidden bg-[#f0edec] p-8 flex flex-col justify-end min-h-[250px]">
+          <div className="relative group rounded-3xl overflow-hidden bg-[#f0edec] p-8 flex flex-col justify-end min-h-[180px] md:min-h-[250px]">
             <img
               className="absolute inset-0 w-full h-full object-cover group-hover:scale-110 transition-transform duration-1000"
               src="https://lh3.googleusercontent.com/aida-public/AB6AXuAF0ZY4x6XRIqBRFpPi2UlQzoWTr5n21MpPJMK0UawRlHFtKGWgZ5dzwHeXumqS3XrLn95Bri4fE7awQIwLbfd75JZpRtmM_crGWjg5EQSIZse-DNdm2IyDrJviw1wAFyuHkHudGujQcbOKezlF0f1xEfCShxOor6oCur5WTRhp3SjzxYg1BIHGEdbyOOSp0MYXQBcf5OqiH9KJJ3RXelevszTFLHxctBfXjbZZLdV4gPv0t-mQOqalMESgL_CFqL3cbbfVlYcVOiE"
@@ -51,7 +51,7 @@ export function BestSellers() {
           </div>
 
           {/* Milk Sarbath */}
-          <div className="relative group rounded-3xl overflow-hidden bg-[#f0edec] p-8 flex flex-col justify-end min-h-[250px]">
+          <div className="relative group rounded-3xl overflow-hidden bg-[#f0edec] p-8 flex flex-col justify-end min-h-[180px] md:min-h-[250px]">
             <img
               className="absolute inset-0 w-full h-full object-cover group-hover:scale-110 transition-transform duration-1000"
               src="https://lh3.googleusercontent.com/aida-public/AB6AXuBDNe7cDN668GpET0_Gpb4bkDwBJ62oXT8ZdnIY3lVpwLLvF_1h0UNU86DDpmwBBoxiiJPn5rW2nCbfQGa0G345_lNTLedQUs3Q50J4d-XNx3hP6WiYHif-0EmYGoweAiAwIf3PlgFLMnEcjYPzF9X86ELq728qR5DaVBDZuYs_B2uE5tAQPtj3x0-Br_S9j3Sc7UPeidyw1WvmppGYqO_mJDnbdt0qNdGlaRXZz0EeW2SXAdUh6wa509JObC7lc_V2UcqwdF0q2ng"
@@ -67,7 +67,7 @@ export function BestSellers() {
           </div>
 
           {/* Watermelon Splash - wide card */}
-          <div className="md:col-span-2 relative group rounded-3xl overflow-hidden bg-[#f0edec] p-8 flex flex-col justify-end min-h-[250px]">
+          <div className="md:col-span-2 relative group rounded-3xl overflow-hidden bg-[#f0edec] p-8 flex flex-col justify-end min-h-[180px] md:min-h-[250px]">
             <div className="absolute inset-0 bg-[#F8F8F8]"></div>
             <div className="relative z-10 flex flex-col md:flex-row md:items-center justify-between gap-6">
               <div>
